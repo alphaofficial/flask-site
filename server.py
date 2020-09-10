8cd0fedd8c525fa0a29d6b956957bec6d14e714c
Albert Alpha <baronofficial@gmail.com>
2020-09-10 00:10:52 +0000

    initial commit

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -7,10 +11,47 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/youtube")
+def youtube():
+    URL = 'http://kworb.net/youtube'
+    page = requests.get(URL)
 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tbl =soup.find("table",{"id":"youtuberealtime"})
+
+    df = pd.read_html(str(tbl))[0]
+    parsed = df.to_dict('records')
+
+    return jsonify(parsed)
+
+
+@app.route("/spotifyartist")
+def spotifyartist():
+    URL = 'http://kworb.net/spotify'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tbl =soup.find("table",{"id":"artisttable"})
+
+    df = pd.read_html(str(tbl))[0]
+    parsed = df.to_dict('records')
+
+    return jsonify(parsed)
+
+"""
+@app.route("/itunes")
+def itunes():
+    URL = 'http://kworb.net/pop'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tbl =soup.find("table", {"class":"sortable popbars"})
+
+    df = pd.read_html(str(tbl))[0]
+    parsed = df.to_dict('records')
+    print(parsed)
+    return jsonify(parsed)
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
